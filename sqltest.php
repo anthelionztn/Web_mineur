773f3d161f5e1e9dd58b091ed6753af4d8e5a455
zhangtunan <zhangtunan@gmail.com>
2015-02-24 09:22:32 +0100

    un peu change

--- sqltest.php
+++ sqltest.php
@@ -1,6 +1,5 @@
  <?php
-    echo "This is a test</br>"; 
-    echo "asdfasdfadsf";
+    echo "This is a test!"; 
     $mysql_server_name="localhost"; //数据库服务器名称
     $mysql_username="user"; // 连接数据库用户名
     $mysql_password="user"; // 连接数据库密码
@@ -11,42 +10,42 @@
                         $mysql_password);
                         
      // 从表中提取信息的sql语句
-    $strsql="SELECT * FROM `dazibaoneirong`";
+    $strsql="SELECT * FROM `dazibao`";
     // 执行sql查询
     $result=mysql_db_query($mysql_database, $strsql, $conn);
     // 获取查询结果
     $row=mysql_fetch_row($result);
     
      
-    echo '<font face="verdana">';
-    echo '<table border="1" cellpadding="1" cellspacing="2">';
+    //echo '<font face="verdana">';
+    //echo '<table border="1" cellpadding="1" cellspacing="2">';
 
     // 显示字段名称
-    echo "</b><tr></b>";
+    //echo "</b><tr></b>";
     for ($i=0; $i<mysql_num_fields($result); $i++)
     {
-      echo '<td bgcolor="#000F00"><b>'.
+      //echo '<td bgcolor="#000F00"><b>'.
       mysql_field_name($result, $i);
-      echo "</b></td></b>";
+      //echo "</b></td></b>";
     }
-    echo "</tr></b>";
+    //echo "</tr></b>";
     // 定位到第一条记录
     mysql_data_seek($result, 0);
     // 循环取出记录
     while ($row=mysql_fetch_row($result))
     {
-      echo "<tr></b>";
+      //echo "<tr></b>";
       for ($i=0; $i<mysql_num_fields($result); $i++ )
       {
-        echo '<td bgcolor="#00FF00">';
+        //echo '<td bgcolor="#00FF00">';
         echo $row[$i];
-        echo '</td>';
+        //echo '</td>';
       }
-      echo "</tr></b>";
+      //echo "</tr></b>";
     }
    
-    echo "</table></b>";
-    echo "</font>";
+    //echo "</table></b>";
+    //echo "</font>";
     // 释放资源
     mysql_free_result($result);
     // 关闭连接
